e475a7df177f0b0e98a5a063780195c347b72bef
falahlaz <alfalahlazuardi@gmail.com>
2020-05-09 17:39:48 +0700

    Adding table relationship

--- app/Space.php
+++ app/Space.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Space extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function photo()
+    {
+        return $this->hasMany(SpacePhoto::class, 'space_id', 'id');
+    }
 }
--- app/SpacePhoto.php
+++ app/SpacePhoto.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class SpacePhoto extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function space()
+    {
+        return $this->belongsTo(Space::class, 'space_id', 'id');
+    }
 }
