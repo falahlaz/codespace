ad1fc6c8359a667b86d88df700961bfed644c513
falahlaz <alfalahlazuardi@gmail.com>
2020-05-09 23:08:31 +0700

    Adding link to space

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a href="{{ route('space.index') }}" class="nav-link">Space</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
