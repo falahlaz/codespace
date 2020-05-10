fd97a40705ffa681e898c0e852a5bf2a105812ff
falahlaz <alfalahlazuardi@gmail.com>
2020-05-11 00:38:10 +0700

    Adding here maps to mapContainer

---
+++ public/js/here.js
@@ -0,0 +1,34 @@
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+        localCoord  = position.coords;
+        objLocalCoord = {
+            lat : localCoord.latitude,
+            lng : localCoord.longitude
+        }
+
+        // Initialize the platform object:
+        let platform = new H.service.Platform({
+            'apikey': window.hereApiKey
+        });
+
+        // Obtain the default map types from the platform object
+        let defaultLayers = platform.createDefaultLayers();
+
+        // Instantiate (and display) a map object:
+        var map = new H.Map(
+            document.getElementById('mapContainer'),
+            defaultLayers.vector.normal.map,
+            {
+                zoom: 13,
+                center: objLocalCoord,
+                pixelRatin: window.devicePixelRatio || 1
+            });
+            window.addEventListener('resize', () => map.getViewPort().resize());
+        
+        let ui          = H.ui.UI.createDefault(map, defaultLayers);
+        let mapEvents   = new H.mapevents.MapEvents(map);
+        let behavior    = new H.mapevents.Behavior(mapEvents);
+    })
+} else {
+    console.error("Goelocation is not supported by this browser!");
+}
