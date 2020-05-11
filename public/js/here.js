dc9c5078a6b9240c8aa498233d238f2fc012e48e
Falahlaz <alfalahlazuardi@gmail.com>
2020-05-11 11:01:31 +0700

    Adding addDragableMarker function

--- public/js/here.js
+++ public/js/here.js
@@ -1,4 +1,4 @@
-if(navigator.geolocation) {
+if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
         localCoord  = position.coords;
         objLocalCoord = {
@@ -28,6 +28,75 @@ if(navigator.geolocation) {
         let ui          = H.ui.UI.createDefault(map, defaultLayers);
         let mapEvents   = new H.mapevents.MapEvents(map);
         let behavior    = new H.mapevents.Behavior(mapEvents);
+
+        // Dragable Marker Function
+        function addDragableMarker(map, behavior) {
+            let inputLat    = document.getElementById('lat')
+            let inputLng    = document.getElementById('lng')
+
+            if (inputLat.value != '' && inputLng.value != '') {
+                objLocalCoord = {
+                    lat : inputLat.value,
+                    lng : inputLng.value
+                }
+            }
+
+            let marker  = new H.map.Marker(objLocalCoord, {
+                volatility: true
+            })
+
+            marker.draggable    = true;
+            map.addObject(marker);
+
+            // disabled the default draggablity of the underlying map
+            // and calculate the offset between mouse and target's position
+            // when starting to drag a marker obj
+            map.addEventListener('dragstart', function(ev) {
+                let target  = ev.target,
+                    pointer = ev.currentPointer;
+                if (target instanceof H.map.Marker) {
+                    let targetPosition  = map.geoToScreen(target.getGeometry());
+                    target['offset']    = new H.math.Point(
+                        pointer.viewportX - targetPosition.x, pointer.viewportY - targetPosition.y
+                    )
+                    behavior.disable()
+                }
+            }, false);
+
+            // Listen to the drag event and move the position of the marker
+            // as necesssary
+            map.addEventListener('drag', function(ev) {
+                let target  = ev.target,
+                    pointer = ev.currentPointer;
+                if (target instanceof H.map.Marker) {
+                    target.setGeometry(
+                        map.screenToGeo(
+                            pointer.viewportX - target['offset'].x, pointer.viewportY - target['offset'].y
+                        )
+                    );
+                }
+            }, false)
+
+            // re-enablethe default draggability of the underlying map
+            // when dragging has completed
+            map.addEventListener('dragend', function(ev) {
+                let target  = ev.target;
+                if (target instanceof H.map.Marker) {
+                    behavior.enable()
+                    let resultCoord = map.screenToGeo(
+                        ev.currentPointer.viewportX,
+                        ev.currentPointer.viewportY
+                    );
+                    // console.log(resultCoord)
+                    inputLat.value  = resultCoord.lat;
+                    inputLng.value  = resultCoord.lng;
+                }
+            }, false)
+        }
+
+        if (window.action == "submit") {
+            addDragableMarker(map, behavior)
+        }
     })
 } else {
     console.error("Goelocation is not supported by this browser!");
